df4d5909327dd5ba0c2fe88981df1b7a5280a422
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-06-08 21:44:48 +0700

    update llaji giao diện trang chủ

--- src/app/admin/category-admin/admin-category-form-add/admin-category-form-add.component.ts
+++ src/app/admin/category-admin/admin-category-form-add/admin-category-form-add.component.ts
@@ -1,4 +1,7 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../../../services/category/category.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-category-form-add',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-category-form-add.component.css']
 })
 export class AdminCategoryFormAddComponent implements OnInit {
-
-  constructor() { }
+  cateForm: FormGroup;
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router
+  ) {
+    this.cateForm =new FormGroup({
+      name: new FormControl('',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(32)
+      ])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    const submitData = this.cateForm.value;
+    return this.categoryService.addCate(submitData).subscribe(data => {
+      this.router.navigateByUrl('/admin/category')
+    })
   }
 
 }
