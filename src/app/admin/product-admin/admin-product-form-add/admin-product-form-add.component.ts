8ed09b9a2273ad069625008f2dd3ab1f2acae949
thanhdat274 <nguyenthanhdat27042002@gmail.com>
2022-06-18 10:17:48 +0700

    làm phần up ảnh

--- src/app/admin/product-admin/admin-product-form-add/admin-product-form-add.component.ts
+++ src/app/admin/product-admin/admin-product-form-add/admin-product-form-add.component.ts
@@ -5,6 +5,7 @@ import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators }
 import { Router, ActivatedRoute } from '@angular/router';
 import { CategoryType } from 'src/app/type/category';
 import { UploadService } from 'src/app/services/upload/upload.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-product-form-add',
@@ -15,12 +16,14 @@ export class AdminProductFormAddComponent implements OnInit {
 
   productForm: FormGroup;
   category: CategoryType[];
-  file: any
+  img: string;
+  productData: any
   constructor(
     private productService: ProductService,
     private router: Router,
-    private categoryService:CategoryService,
+    private categoryService: CategoryService,
     private uploadImg: UploadService,
+    private http: HttpClient
   ) {
     this.productForm = new FormGroup({
       name: new FormControl('', [
@@ -28,45 +31,65 @@ export class AdminProductFormAddComponent implements OnInit {
         Validators.minLength(6),
         Validators.maxLength(100)
       ]),
-      price: new FormControl('',[
+      price: new FormControl('', [
         Validators.required
       ]),
-      sale_price: new FormControl('',[
+      sale_price: new FormControl('', [
         Validators.required
       ]),
-      quantity: new FormControl('',[
+      quantity: new FormControl('', [
         Validators.required
       ]),
-      short_desc: new FormControl('',[
+      short_desc: new FormControl('', [
         Validators.required
       ]),
-      desc: new FormControl('',[
+      desc: new FormControl('', [
         Validators.required
       ]),
-      img: new FormControl('',[
-        Validators.required
-      ]),
+      img: new FormControl(''),
       categoryId: new FormControl(0)
     })
-    this.category =[]
+    this.category = []
+    this.img = '';
   }
 
   ngOnInit(): void {
-    this.categoryService.listCate().subscribe(data=>{
+    this.categoryService.listCate().subscribe(data => {
       this.category = data
     })
   }
-  onValidateNameProduct(control: AbstractControl): ValidationErrors | null {
-    const { value } = control;
-    if (!value.includes('product')) {
-      return { hasProductError: true };
+
+  onSubmit(): void {
+    console.log(1);
+
+    const CLOUDINARY_PRESET_KEY = "js8yqruv";
+    const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/dvj4wwihv/image/upload";
+
+    if (this.img) {
+      const formData = new FormData();
+      formData.append("file", this.img);
+      formData.append("upload_preset", CLOUDINARY_PRESET_KEY);
+      this.http.post(CLOUDINARY_API_URL, formData).subscribe((data: any) => {
+        this.productData = {
+          categoryId: this.productForm.value.categoryId,
+          name: this.productForm.value.name,
+          price: this.productForm.value.price,
+          sale_price: this.productForm.value.price,
+          quantity: this.productForm.value.quantity,
+          short_desc: this.productForm.value.short_desc,
+          img: data.url,
+          desc: this.productForm.value.desc
+
+        }
+        console.log(this.productData);
+        this.productService.addProducts(this.productData).subscribe(data => {
+          this.router.navigateByUrl('/admin/products')
+        })
+      })
     }
-    return null;
   }
-  onSubmit() {
-    const submitData = this.productForm.value;
-    return this.productService.addProducts(submitData).subscribe(data => {
-      this.router.navigateByUrl('/admin/products')
-    })
+  upload(event: any) {
+    this.img = event.target.files[0];
+    console.log(this.img);
   }
 }
